0cdae12b27916eeadd279f3941fad69eae97dbc8
Braden Wiggins <braden@fractal-hq.com>
2025-03-05 13:03:39 -0500

    feat: add jsr

--- demo.ts
+++ demo.ts
@@ -2,37 +2,39 @@ import { ansiGradient, CLEAR as C } from './src'
 import { log, logger } from './src'
 import { r, d } from './src'
 
-const l = logger({ prefix: d('| ') })
+if (import.meta.main) {
+	const l = logger({ prefix: d('| ') })
 
-// Create a gradient function.
-const g = ansiGradient('#38b2db', '#5959b5', '#e84067')
+	// Create a gradient function.
+	const g = ansiGradient('#38b2db', '#5959b5', '#e84067')
 
-// Wrap a string in the gradient.
-const text = g('Simple gradient text.')
+	// Wrap a string in the gradient.
+	const text = g('Simple gradient text.')
 
-l(text)
+	l(text)
 
-// Or use color stops.
-const fade = `
+	// Or use color stops.
+	const fade = `
 ${g(0.5)}■■■■■■■■■■■■${C}
 ${g(0.0)}■■■■■■■■■■■■${C}
 ${g(0.9)}■■■■■■■■■■■■${C}`
 
-l(fade)
+	l(fade)
 
-function test_log() {
-	const e = new Error('💥', { cause: d('¯\\_(ツ)_/¯') })
-	log()
-	log(r(e.name))
+	function test_log() {
+		const e = new Error('💥', { cause: d('¯\\_(ツ)_/¯') })
+		log()
+		log(r(e.name))
 
-	log()
-	log([e.message, { cause: e.cause }])
+		log()
+		log([e.message, { cause: e.cause }])
 
-	const a = [1, 2, { a: 1, b: 2 }]
-	log()
-	log(a, { delimiter: d(', ') })
-	log()
-	log([a], { delimiter: d(', ') })
+		const a = [1, 2, { a: 1, b: 2 }]
+		log()
+		log(a, { delimiter: d(', ') })
+		log()
+		log([a], { delimiter: d(', ') })
+	}
+
+	test_log()
 }
-
-test_log()
